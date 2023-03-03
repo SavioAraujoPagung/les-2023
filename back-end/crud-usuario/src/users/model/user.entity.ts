39ec78727208c2e302bc928ee97f8968d2302d48
Danilo Rossmann <danilo.rossmann@freterapido.com>
2023-03-03 13:35:58 -0300

    crud-usuario e login

---
+++ back-end/crud-usuario/src/users/model/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  gender: string;
+
+  @Column()
+  office: string;
+
+  @Column()
+  password: string;
+}
