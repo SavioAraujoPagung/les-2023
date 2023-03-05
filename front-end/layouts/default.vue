8a7d052a2b2aededdf8e7ef775a4e4e8c6e252d8
Mikael Botassi de Oliveira <elkamilive@gmail.com>
2023-03-05 19:14:22 -0300

    Adicionando Layout

---
+++ front-end/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <Sidebar />
+    <main class="main-content d-flex flex-column align-items-center justify-content-center">
+        <div class="container shadow-lg border-1 p-5">
+            <slot></slot>
+        </div>
+    </main>
+</template>
--- front-end/pages/Login.vue
+++ front-end/pages/Login.vue
@@ -1,1 +1,15 @@
-temp
+<template>
+    <h1>Ola Mundo</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+definePageMeta({
+    layout: false
+});
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
