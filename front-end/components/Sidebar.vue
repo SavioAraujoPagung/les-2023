ab0482b8e1c968fb05f406bf9590e0b3d5ec9675
Mikael Botassi de Oliveira <elkamilive@gmail.com>
2023-03-23 07:33:27 -0300

    sidebar fechando ao clicar nos links

--- front-end/components/Sidebar.vue
+++ front-end/components/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-ligh bg-gradient-primary">
     <div class="container-fluid">
-      <NuxtLink class="navbar-brand p-4" to="/">
+      <NuxtLink @click=toggle class="navbar-brand p-4" to="/">
         <img src="@/assets/img/brand.webp" alt="logo" class="img-fluid">
       </NuxtLink>
       <button class="navbar-toggler text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar"
@@ -12,13 +12,13 @@
         aria-labelledby="sidebarLabel">
         <div class="offcanvas-header">
           <h5 class="offcanvas-title text-dark fw-bold h3" id="sidebarLabel">Admnistrador</h5>
-          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          <button type="button" ref="closeBtn" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
           <ul class="navbar-nav justify-content-end">
             
             <li class="nav-item">
-              <NuxtLink class="nav-link d-flex align-items-center gap-2 text-white" aria-current="page" to="/usuarios">
+              <NuxtLink @click=toggle class="nav-link d-flex align-items-center gap-2 text-white" aria-current="page" to="/usuarios">
                 <i class="icon icon-16">
                   <IconsPersonFill/>
                 </i>
@@ -27,7 +27,7 @@
             </li>
 
             <li class="nav-item">
-              <NuxtLink class="nav-link d-flex align-items-center gap-2 text-white" aria-current="page" to="/clientes">
+              <NuxtLink @click=toggle class="nav-link d-flex align-items-center gap-2 text-white" aria-current="page" to="/clientes">
                 <i class="icon icon-16">
                   <IconsCustomer/>
                 </i>
@@ -36,7 +36,7 @@
             </li>
             
             <li class="nav-item">
-              <NuxtLink class="nav-link d-flex align-items-center gap-2 text-white" aria-current="page" to="/estoque">
+              <NuxtLink @click=toggle class="nav-link d-flex align-items-center gap-2 text-white" aria-current="page" to="/estoque">
                 <i class="icon icon-16">
                   <IconsBox/>
                 </i>
@@ -45,7 +45,7 @@
             </li>
 
             <li class="nav-item">
-              <NuxtLink class="nav-link d-flex align-items-center gap-2 text-white" aria-current="page" to="/produtos">
+              <NuxtLink @click=toggle class="nav-link d-flex align-items-center gap-2 text-white" aria-current="page" to="/produtos">
                 <i class="icon icon-16">
                   <IconsBag/>
                 </i>
@@ -70,9 +70,39 @@
 
 <script setup>
 
+  const closeBtn = ref(null);
+  const isOpen = ref(false);
+  const toggle = () => closeBtn.value.click();
+
   const logout = () => {
     navigateTo('login');
     localStorage.removeItem("isLogged");
   }
 
 </script>
+
+<style scoped>
+
+  li.nav-item{
+    position: relative;
+  }
+  li.nav-item::after{
+    content: "";
+    position: absolute;
+    background-color: #fff;
+    border-radius: 1rem;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    height: 0;
+    width: 5px;
+    transition: all 0.1s ease-in-out;
+  }
+
+  li.nav-item:hover::after{
+    height: 70%;
+    transition: all 0.1s ease-in-out;
+
+  }
+
+</style>
