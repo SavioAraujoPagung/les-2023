84eb768a9fdd73e1a1e40178ed32305652d79e8e
savioaraujopagung <savio.pagung@hotmail.com>
2023-04-03 01:12:29 -0300

    buscar valor consumido

--- back-end/crud/src/consumption/controller/consumption.controller.ts
+++ back-end/crud/src/consumption/controller/consumption.controller.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Consumption } from '../model/Consumption.entity';
 import { ProductConsumption } from 'src/productConsumption/model/ProductConsumption.entity';
-
+import { CheckIn } from 'src/checkin/model/checkin.entity';
 @Controller('consumption')
 export class ConsumptionController {
   private logger: Logger;
@@ -12,7 +12,10 @@ export class ConsumptionController {
     private readonly repository: Repository<Consumption>,
 
     @InjectRepository(ProductConsumption)
-    private readonly product: Repository<ProductConsumption>
+    private readonly product: Repository<ProductConsumption>,
+
+    @InjectRepository(CheckIn)
+    private readonly checkin: Repository<CheckIn>,
 
     ) {
       this.logger = new Logger('CustomerControllerRepository');
@@ -28,7 +31,30 @@ export class ConsumptionController {
       return this.repository.save(consumption);
     } catch (error) {
       this.logger.error(`Não foi possivel cadastrar um cliente. ${error}`);
-      throw new BadRequestException('Impossível registar consumo de produto!');
+      throw new BadRequestException('Cliente não encontrado!');
+    }
+   
+  } 
+
+  @Get(':rfid')
+  async valueConsumption(@Param('rfid', ParseIntPipe) rfid: string): Promise<string> {
+    try {
+      console.log(rfid)
+      const checkin = await this.getRridOnline(rfid)
+      console.log(checkin)
+      const consumptions = await this.repository.find({where: { checkin: { id: checkin.id } },})
+      console.log(consumptions)
+
+      let value = 0
+
+      for (let i = 0; i < consumptions.length; i++) {
+        value += consumptions[i].price;
+      }
+      
+      return value.toString()
+    } catch (error) {
+      this.logger.error(`Não foi possivel cadastrar um cliente. ${error}`);
+      throw new BadRequestException('Impossível consultar valor consumido!');
     }
    
   } 
@@ -54,5 +80,22 @@ export class ConsumptionController {
     if(userDeleted.affected > 0){
       return `Cliente ${id} deletado com sucesso!`;
     }
+  }
+
+  async getRridOnline(filter: string): Promise<CheckIn> {
+    const checkIn = await this.checkin.find({
+      relations: ['customer'],
+      where: [
+        { customer: { rfid: filter } }, 
+      ],
+    });
+    
+    for (let i = 0; i < checkIn.length; i++) {
+      if (!checkIn[i].pago) {
+        return checkIn[i];
+      }
+    };
+
+    throw new NotFoundException("Não há checkin para este RFID!");
   }
 }
--- back-end/crud/src/consumption/modules/consumption.module.ts
+++ back-end/crud/src/consumption/modules/consumption.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConsumptionController } from '../controller/consumption.controller';
 import { Consumption } from '../model/consumption.entity';
 import { ProductConsumption } from 'src/productConsumption/model/ProductConsumption.entity';
+import { CheckIn } from 'src/checkin/model/checkin.entity';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Consumption, ProductConsumption])],
+  imports: [TypeOrmModule.forFeature([Consumption, ProductConsumption, CheckIn])],
   controllers: [ConsumptionController],
 })
 export class ConsumptionModule {}
